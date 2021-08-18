65dd1adb550958ba60bb9d9e518a14a0e2124fab
dlstodyo <dudgks3316@naver.com>
2021-08-18 23:52:26 +0900

    K번째수

---
+++ 프로그래머스/K번째수.js
@@ -0,0 +1,9 @@
+// K번째 수
+
+const solution = (array, commands) => {
+
+}
+
+const array = [1, 5, 2, 6, 3, 7, 4];
+const commands = [[2, 5, 3], [4, 4, 1], [1, 7, 3]];
+console.log(solution(array, commands));
