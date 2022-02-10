9f4209543a5e770f7239e7f219b998151f728204
dlstodyo <dudgks3316@naver.com>
2022-02-10 23:51:03 +0900

    1단계

---
+++ 프로그래머스/1단계/숫자 문자열과 영단어.js
@@ -0,0 +1,20 @@
+function solution(s) {
+        
+    var answer = s.replace(/(zero|one|two|three|four|five|six|seven|eight|nine)/g, function(change){
+        
+        switch(change) {
+            case 'zero' : return 0;
+            case 'one' : return 1;
+            case 'two' : return 2;
+            case 'three' : return 3;
+            case 'four' : return 4;
+            case 'five' : return 5;
+            case 'six' : return 6;
+            case 'seven' : return 7;
+            case 'eight' : return 8;
+            case 'nine' : return 9;
+        }
+    })
+    
+    return parseInt(answer);
+}
