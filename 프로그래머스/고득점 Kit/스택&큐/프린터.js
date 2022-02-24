685c5c7b895942daadaf62b815627154e8f2a4ef
dlstodyo <dudgks3316@naver.com>
2022-02-24 23:40:37 +0900

    스택

---
+++ 프로그래머스/고득점 Kit/스택&큐/프린터.js
@@ -0,0 +1,24 @@
+function solution(priorities, location) {
+
+    let maxNum;
+    let i = 0;
+
+    while(true) {
+        maxNum = Math.max.apply(null, priorities);
+        let j = priorities.shift();
+        
+        if(j === maxNum) {
+            i++;
+            if(location === 0) {
+                return i;
+            }
+        } else {
+            priorities.push(j);
+        }
+        location--;
+
+        if (location == -1) {
+            location = priorities.length - 1;
+        }
+    }
+}
