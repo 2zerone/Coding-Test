100563b37e7b56b640c58b4e8599cca958ee6bef
dlstodyo <dudgks3316@naver.com>
2022-06-23 21:42:33 +0900

    Level 1

--- 프로그래머스/모든 문제/Level 1/소수 찾기.js
+++ 프로그래머스/모든 문제/Level 1/소수 찾기.js
@@ -1,16 +1,35 @@
+// 통과 안되는 코드임
+// function solution(n) {
+//     var answer = 0;
+//     let count = 0;
+    
+//     for(let i=2; i<=n; i++) {
+//         for(let j=2; j<=i; j++) {
+//             if(i % j === 0) count++;
+//             }
+//         if(count === 1) {
+//             answer++;
+//         }
+//         count = 0;
+//     }
+    
+//     return answer;
+// }
+
 function solution(n) {
-    var answer = 0;
-    let count = 0;
-    
-    for(let i=2; i<=n; i++) {
-        for(let j=2; j<=i; j++) {
-            if(i % j === 0) count++;
-            }
-        if(count === 1) {
-            answer++;
-        }
-        count = 0;
+    let answer = 0;
+
+    const arr = new Array(n+1).fill(true);
+    const range = Math.sqrt(n);
+
+    for(let i=2; i<range+1; i++) {
+        if(arr[i] === false) continue;
+        for(let j=i*i; j<n+1; j+=i) arr[j] = false;
     }
-    
+
+    for(let i=2; i<n+1; ++i) {
+        if(arr[i] === true) answer++;
+    }
+
     return answer;
 }
