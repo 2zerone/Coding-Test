bccd1dce71f1ebffdc99cfc711aae9f40f7b97b6
dlstodyo <dudgks3316@naver.com>
2022-06-30 19:56:14 +0900

    Level 1 (새로운 풀이)

--- 프로그래머스/모든 문제/Level 1/문자열 내 마음대로 정렬하기.js
+++ 프로그래머스/모든 문제/Level 1/문자열 내 마음대로 정렬하기.js
@@ -4,3 +4,18 @@ function solution(strings, n) {
 }
 
 // 가장 좋아요가 많은 풀이와 똑같이 풀었다!!
+
+function solution(strings, n) {
+    return strings.sort((a, b) => {
+        const chr1 = a.charAt(n);
+        const chr2 = b.charAt(n);
+
+        if (chr1 == chr2) {
+            return (a > b) - (a < b);
+        } else {
+            return (chr1 > chr2) - (chr1 < chr2);
+        }
+    })
+}
+
+// 새로운 접근법 (sort 함수 안에 다양한 식들이 있을 수 있다.)
