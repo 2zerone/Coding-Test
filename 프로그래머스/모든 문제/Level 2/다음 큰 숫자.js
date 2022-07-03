3ddb3f4e1dd8d76bc2faadb8149fff5f1e11f5ef
dlstodyo <dudgks3316@naver.com>
2022-07-03 21:37:22 +0900

    다음 큰 숫자

---
+++ 프로그래머스/모든 문제/Level 2/다음 큰 숫자.js
@@ -0,0 +1,21 @@
+function solution(n) {
+    let answer;
+
+    let oneCount = 0;
+    const binary = n.toString(2);
+    for(let x of binary) if(x === '1') oneCount++;
+
+    let newOneCount = 0;
+    for(let i=n+1; i<1000000; i++) {
+        let newBinary = i.toString(2);
+        for(let x of newBinary) {
+            if(x === '1') newOneCount++;
+        }
+        if(oneCount === newOneCount) {
+            answer = i;
+            break;
+        } else newOneCount = 0;
+    }
+
+    return answer;
+}
