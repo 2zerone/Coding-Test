0b8c6c94a3d322055a6464a6b6447de63455092d
dlstodyo <dudgks3316@naver.com>
2022-08-05 18:57:27 +0900

    Level 2

---
+++ 프로그래머스/모든 문제/Level 2/JadenCase 문자열 만들기.js
@@ -0,0 +1,8 @@
+function solution(s) {
+    s = s.toLowerCase().split('');
+    s[0] = s[0].toUpperCase();
+    for(let i=0; i<s.length-1; i++) {
+        if(s[i] === ' ') s[i+1] = s[i+1].toUpperCase();
+    }
+    return s.join('');
+}
