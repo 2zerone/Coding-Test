803360ffc16401ebf4e7fc85f92a299aa8a1d54d
2zerone <dudgks3316@naver.com>
2022-09-26 20:11:03 +0900

    Easy

--- LeetCode/14. Longest Common Prefix.js
+++ LeetCode/14. Longest Common Prefix.js
@@ -1,28 +1,18 @@
 var longestCommonPrefix = function(strs) {
-    let result = '';
-    if(strs.length <1) return result;
-    const first = strs[0]; // flower
-    for(let i = 0; i<first.length ; i++){
-        const c = first[i]; // f, l, o, w, e, r 
-        
-        for(let j = 1;j<strs.length;j++){
-            if(strs[j][i] !== c) return result; // f"l"ow, fl"o"w
-        }
-        result += c;
-    }
-    return result;
-};
-
-var longestCommonPrefix = function(strs) {
-    for (let i = 0; i < strs.length; i++) {
-        for (let j = 0; j < strs[i].length; j++) {
-        // console.log(strs[i+2][j]);
-          if (strs[i][j] == strs[i + 1][j] && strs[i + 1][j] ==strs[i + 2][j]) {
-            return (strs[i][j]);
-
-          } else {
-            return "0";
-          }
-        }
-     }
+  let answer = '';
+  
+  if(strs.length < 1) return answer;
+  const firstStr = strs[0];
+  
+  for(let i=0; i<firstStr.length; i++) {
+      const initial = firstStr[i];
+      
+      for(let j=0; j<strs.length; j++) {
+          if(strs[j][i] !== initial) return answer;
+      }
+      
+      answer += initial;
+  }
+  
+  return answer;
 };
