0b9ea02d9f71c069976f5b0455470609e07226af
2zerone <dudgks3316@naver.com>
2022-11-15 20:51:10 +0900

    D2

---
+++ python/간단한 369 게임.py
@@ -0,0 +1,9 @@
+n = int(input())
+for i in range(1, n+1):
+    i = str(i)
+    cnt = i.count('3') + i.count('6') + i.count('9')
+
+    if cnt == 0:
+        print(i, end=' ')
+    else:
+        print(cnt, end=' ')
