a24fa3c7f36b7038c75e95c898deeaeb334c2212
dlstodyo <dudgks3316@naver.com>
2022-11-17 20:34:06 +0900

    D2 (다른 코드 추가)

--- python/파스칼의 삼각형.py
+++ python/파스칼의 삼각형.py
@@ -13,3 +13,20 @@ for i in range(1, A+1):
         answer.append(tmp)
     for l in answer:
         print(*l)
+
+// 다른 코드
+T=int(input())
+ 
+for i in range(T):
+    N=int(input())
+    print('#%d'%(i+1))
+    for j in range(N):
+        if j==0:
+            print("1")
+        elif j==1:
+            print("1 1")
+        else:
+            print("1",end=' ')
+            for k in range(j-1):
+                print(j,end=' ')
+            print("1")
