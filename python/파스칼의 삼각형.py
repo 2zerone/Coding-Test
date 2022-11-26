3477e96614b7a561f50125efe621d10c21f10264
dlstodyo <dudgks3316@naver.com>
2022-11-26 23:58:51 +0900

    D2 complete

--- python/파스칼의 삼각형.py
+++ python/파스칼의 삼각형.py
@@ -14,19 +14,19 @@ for i in range(1, A+1):
     for l in answer:
         print(*l)
 
-// 다른 코드
-T=int(input())
+# // 다른 코드
+# T=int(input())
  
-for i in range(T):
-    N=int(input())
-    print('#%d'%(i+1))
-    for j in range(N):
-        if j==0:
-            print("1")
-        elif j==1:
-            print("1 1")
-        else:
-            print("1",end=' ')
-            for k in range(j-1):
-                print(j,end=' ')
-            print("1")
+# for i in range(T):
+#     N=int(input())
+#     print('#%d'%(i+1))
+#     for j in range(N):
+#         if j==0:
+#             print("1")
+#         elif j==1:
+#             print("1 1")
+#         else:
+#             print("1",end=' ')
+#             for k in range(j-1):
+#                 print(j,end=' ')
+#             print("1")
