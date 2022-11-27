0aa136c9061033688b9682977f1bb5bc10646b97
dlstodyo <dudgks3316@naver.com>
2022-11-27 22:34:39 +0800

    Level 0

---
+++ 프로그래머스/모든 문제/Level 0/다음에 올 숫자.js
@@ -0,0 +1,12 @@
+function solution(common) {
+    let answer = 0;
+    
+    common[2] - common[1] === common[1] - common[0]
+    ?
+    answer = common[common.length-1] + (common[1] - common[0])
+    :
+    answer = common[common.length-1] * (common[1] / common[0])
+    
+    
+    return answer;
+}
