76296ac963809dae1dcc668a235e2cdcbc81404b
2zerone <dudgks3316@naver.com>
2022-12-29 20:30:24 +0900

    Level 0

--- 프로그래머스/모든 문제/Level 0/대문자와 소문자.js
+++ 프로그래머스/모든 문제/Level 0/대문자와 소문자.js
@@ -1,6 +1,13 @@
+// 아스키
 function solution(my_string) {
     return [...my_string].map((v) =>
                        v.charCodeAt() > 96
                        ? v.toUpperCase()
                        : v.toLowerCase()).join('');
+}
+
+function solution(my_string) {
+    var answer = '';
+    for (let c of my_string) answer += c === c.toLowerCase() ? c.toUpperCase() : c.toLowerCase();
+    return answer;
 }
