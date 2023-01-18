3bf5449e7e9477c2ee8bf894309bb4f8ca9514df
dlstodyo <dudgks3316@naver.com>
2023-01-18 22:10:35 +0900

    Level 0(수정)

--- 프로그래머스/모든 문제/Level 0/369 게임.js
+++ 프로그래머스/모든 문제/Level 0/369 게임.js
@@ -1,10 +1,3 @@
 function solution(order) {
-    let count = 0;
-
-    let newOrder = order.toString().split('');
-    for(let x of newOrder) {
-        if(x % 3 === 0) count++;
-    }
-    
-    return count;
+    return order.toString().split('').filter((v) => v==='3' || v==='6' || v==='9').length;
 }
