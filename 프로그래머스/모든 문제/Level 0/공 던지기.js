29a7cc55118a11d6f2e829011dc2aafc2628207b
2zerone <dudgks3316@naver.com>
2023-04-03 23:13:41 +0900

    progress(review)

--- 프로그래머스/모든 문제/Level 0/공 던지기.js
+++ 프로그래머스/모든 문제/Level 0/공 던지기.js
@@ -8,3 +8,29 @@ function solution(numbers, k) {
     
     return numbers[answer - 2];
 }
+
+// progress
+function solution(numbers, k) {
+    let one_array = [];
+    let two_array = [];
+    let answer = 0;
+    
+    if(numbers.length % 2 === 0) {
+        for(let i=0; i<numbers.length; i+=2) {
+            one_array.push(numbers[i]);
+        }
+        
+        answer = one_array[(k-1) % one_array.legth];
+    } else {
+        for(let i=0; i<numbers.length; i+=2) {
+            two_array.push(numbers[i]);
+        }
+        for(let j=1; j<numbers.length; j+=2) {
+            two_array.push(numbers[j]);
+        }
+        
+        answer = two_array[(k-1) % two_array.length];
+    }
+    
+    return answer;
+}
