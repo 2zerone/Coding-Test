2b037553bef46cefed2dcd126f488e4600650e72
dlstodyo <dudgks3316@naver.com>
2023-08-13 23:06:18 +0900

    complete

---
+++ 프로그래머스/모든 문제/코딩 기초 트레이닝/공백으로 구분하기 2.js
@@ -0,0 +1,9 @@
+const solution = (my_string) => {
+    return my_string.split(' ').filter((v) => v !== '');
+}
+
+/*
+const solution = (my_string) => {
+    return my_string.split(' ').filter((v) => v);
+}
+*/
