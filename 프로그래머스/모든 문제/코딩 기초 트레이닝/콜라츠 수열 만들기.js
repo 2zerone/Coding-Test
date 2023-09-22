b13fdb75abf7049127c71b8529d761e9e99a4506
dlstodyo <dudgks3316@naver.com>
2023-09-22 23:51:42 +0900

    complete

---
+++ 프로그래머스/모든 문제/코딩 기초 트레이닝/콜라츠 수열 만들기.js
@@ -0,0 +1,22 @@
+const solution = (n) => {
+    const answer = [n];
+
+    while(n>1) {
+        if(n % 2 === 0) {
+            n = n/2;
+            answer.push(n);
+        } else {
+            n = 3*n+1;
+            answer.push(n);
+        }
+    }
+    return answer;
+}
+
+// 재귀
+const solution2 = (n, arr = []) => {
+    arr.push(n)
+    if (n === 1) return arr
+    if (n % 2 === 0) return solution(n / 2, arr)
+    return solution(3 * n + 1, arr)
+}
