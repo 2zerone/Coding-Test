715b22635c04dc9654cbb50066fb3e43baf6c33f
2zerone <dudgks3316@naver.com>
2023-09-23 22:53:02 +0900

    complete

---
+++ 프로그래머스/모든 문제/코딩 기초 트레이닝/수 조작하기2.js
@@ -0,0 +1,27 @@
+// intuitive code
+const solution = (numLog) => {
+    const obj = {
+        '1' : 'w',
+        '-1' : 's',
+        '10' : 'd',
+        '-10' : 'a'
+    }
+
+    const arr = [];
+    for(let i=0; i<numLog.length-1; i++) {
+        arr.push(numLog[i+1] - numLog[i]);
+    }
+
+    return arr.map((v) => obj[v]).join('');
+}
+
+// summary Code
+const solution2 = (numLog) => {
+    const obj = {
+        '1': 'w', '-1': 's', '10': 'd', '-10': 'a'
+    };
+
+    return numLog.slice(1).map((v, i) => {
+        return obj[v - numLog[i]]
+    }).join('')
+}
