7dc83a33ebcd2c1cd738782d4db19a40417a6c20
2zerone <dudgks3316@naver.com>
2023-11-19 23:57:53 +0900

    review

---
+++ 프로그래머스/모든 문제/코딩 기초 트레이닝/배열 비교하기.js
@@ -0,0 +1,6 @@
+function solution(arr1, arr2) {
+    if(arr1.length === arr2.length) {
+        arr1.reduce((acc, v) => acc + v, 0) > arr2.reduce((acc, v) => acc + v, 0) ? );
+    } else if(arr1.length > arr2.length) return 1;
+    else return -1;
+}
